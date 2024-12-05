afb2561d187fa2b0e76e5cbf02a07c53494f4cdf
filliszou <filliszou@tencent.com>
2024-12-05 11:32:31 +0800

    hh4.py upload

---
+++ hh4.py
@@ -0,0 +1,8 @@
+
+def check():
+    password = "abcd@1234"
+    user = input("Enter your password: ")
+    if user == password:
+        print("Access granted")
+    else:
+        print("Access denied")
